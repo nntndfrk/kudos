a10b5007211aa8be4544fa0c9e8619a5e01284c8
Mykhailo Smolinskyi <nntndfrk@gmail.com>
2023-04-23 20:40:37 +0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import {component$} from '@builder.io/qwik';
+// import type { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div class="h-screen w-full bg-slate-600">
+      <h2 class="font-bold text-5xl text-blue-400">TailwindCSS works!</h2>
+    </div>
+  );
+});
